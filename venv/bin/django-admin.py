6ffe9e59ca57d2e324ab75a0020651ac955eeb55
zzaakiirr <zzaakiirr@gmail.com>
2018-08-13 18:05:44 +0300

    Added working files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zzaakiirr/Shopping-Cart-Using-Django-2.0-and-Python-3.6/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
